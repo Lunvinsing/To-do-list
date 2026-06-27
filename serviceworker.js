7c38f75277bb8a23ab5b0f525549a97f87ec149b
Sing <gmlbb82@gmail.com>
2026-06-27 20:39:38 +0700

    Final XD

--- serviceworker.js
+++ serviceworker.js
@@ -1,69 +1,72 @@
-    const CACHE_NAME = 'todo-cache-v1';
+const CACHE_NAME = "todo-cache-v2";
 
-    // Files to save in cache when service worker is installed
-    const ASSETS = [
-    '/',
-    '/index.html',
-    '/script.js',
-    '/css/style.css',
-    '/css/font.css',
-    '/assets/check.png',
-    '/assets/logo.png',
-    '/assets/unchecked.png',
-    '/assets/unchecked.svg',
-    '/fonts/Ubuntu-Regular.ttf',
-    '/fonts/Ubuntu-Bold.ttf',
-    ];
+// Files to save in cache when service worker is installed
+const ASSETS = [
+    "/",
+    "/index.html",
+    "/script.js",
+    "/css/style.css",
+    "/css/font.css",
+    "/assets/check.png",
+    "/assets/logo.png",
+    "/assets/unchecked.png",
+    "/assets/unchecked.svg",
+    "/fonts/Ubuntu-Regular.ttf",
+    "/fonts/Ubuntu-Bold.ttf",
+    "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4",
+    "https://cdn.jsdelivr.net/npm/daisyui@5",
+    "https://cdn.jsdelivr.net/npm/sweetalert2@11",
+];
 
-    /*
-    * Install Event
-    * Runs when the service worker is installed.
-    * Save important files into cache so they can be used offline.
-    */
-    self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
-    });
+/*
+ * Install Event
+ * Runs when the service worker is installed.
+ * Save important files into cache so they can be used offline.
+ */
+self.addEventListener("install", (event) => {
+event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)),
+);
+});
 
-    /*
-    * Activate Event
-    * Runs after installation.
-    * Remove old caches that are no longer used.
-    */
-    self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((keys) =>
-        Promise.all(
-            keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
-        )
-        )
-    );
-    });
+/*
+ * Activate Event
+ * Runs after installation.
+ * Remove old caches that are no longer used.
+ */
+self.addEventListener("activate", (event) => {
+event.waitUntil(
+    caches.keys().then((keys) =>
+        Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)),
+        ),
+    ),
+);
+});
 
-    /*
-    * Fetch Event
-    * Runs every time the app requests a file.
-    *
-    * 1. Check if the file is already in cache.
-    * 2. If found, return the cached file.
-    * 3. If not found, get it from the network.
-    * 4. Save the new file into cache for future use.
-    */
-    self.addEventListener('fetch', (event) => {
+/*
+ * Fetch Event
+ * Runs every time the app requests a file.
+ *
+ * 1. Check if the file is already in cache.
+ * 2. If found, return the cached file.
+ * 3. If not found, get it from the network.
+ * 4. Save the new file into cache for future use.
+ */
+self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then((cached) => {
-        return ( cached || fetch(event.request).then((response) => {
+        return (
+            cached ||
+            fetch(event.request).then((response) => {
             caches.open(CACHE_NAME).then((cache) => {
                 cache.put(event.request, response.clone());
             });
             return response;
             })
         );
-        })
+        }),
     );
-    });
-
+});
 
 //      install → Save app files into cache.
 //      activate → Delete old cache versions.
